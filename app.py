42c5deeed796ee63ec4bdc89a37b402e09e079fe
arnav0103 <66205884+arnav0103@users.noreply.github.com>
2020-08-25 18:15:29 +0530

    u

--- app.py
+++ app.py
@@ -1,12 +1,25 @@
 from Bytes import app , db
 from Bytes.forms import RegistrationForm , LoginForm , UpdateUserForm
-from Bytes.models import User
+from Bytes.models import User , Time , Train
 from picture_handler import add_profile_pic
 from flask import render_template, request, url_for, redirect , flash
 from flask_login import current_user, login_required, login_user , logout_user
-
+import datetime
+from sqlalchemy import asc , desc
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    time = Time.query.get(5)
+    time.start = datetime.datetime(2020, 8, 26, 16, 50)
+    time.end = datetime.datetime(2020, 8, 25, 17, 30)
+    time = Time.query.get(6)
+    time.start = datetime.datetime(2020, 8, 26, 17, 35)
+    time.end = datetime.datetime(2020, 8, 25, 18, 5)
+    time = Time.query.get(7)
+    time.start = datetime.datetime(2020, 8, 26, 18, 10)
+    time.end = datetime.datetime(2020, 8, 25, 18, 45)
+    time = Time.query.get(4)
+    db.session.delete(time)
+    db.session.commit()
     return render_template("index.htm")
 
 @app.route('/register' , methods = ['GET', 'POST'])
@@ -82,6 +95,19 @@ def account():
 
     profile_image = url_for('static', filename= current_user.profile_image)
     return render_template('account.htm', profile_image=profile_image, form=form, pic = pic)
+
+@app.route('/sched/<day>' , methods = ['GET' , 'POST' ])
+@login_required
+def sched(day):
+    time = Time.query.order_by(Time.start.asc())
+    m = []
+    for t in time:
+        print(t.start.strftime('%A'))
+        if t.start.strftime('%A') == day:
+            m.append(t)
+
+    return render_template('sched.htm' , m = m)
+
 
 ###########################################
 
